ce1c024b88edfa44aef332e825d8c863387162d6
Querido-Fue <77534783+Querido-Fue@users.noreply.github.com>
2026-05-10 06:29:27 +0900

    global constants freeze cleanup

--- project/game/script/data/global/global_constants.js
+++ project/game/script/data/global/global_constants.js
@@ -1,13 +1,13 @@
 /**
- * 게임 전반에 걸쳐 사용되는 전역 상수 모음
+ * 게임 전반에 걸쳐 사용되는 전역 상수 모음입니다.
  */
-export const GLOBAL_CONSTANTS = {
-    ASPECT_RATIO: {
+export const GLOBAL_CONSTANTS = Object.freeze({
+    ASPECT_RATIO: Object.freeze({
         WIDTH: 16,
         HEIGHT: 9,
         RATIO: 16 / 9
-    },
-    POOL_WARMUP: {
+    }),
+    POOL_WARMUP: Object.freeze({
         ANIMATOR: 500,
         TEXT: 50,
         SLIDER: 10,
@@ -18,13 +18,13 @@ export const GLOBAL_CONSTANTS = {
         TOGGLE: 10,
         CANVAS_2D: 16,
         CANVAS_WEBGL: 8
-    },
-    GAME_VERSION: "0.41",
+    }),
+    GAME_VERSION: '0.41',
     WEBGL_MAX_SPRITES: 16000,
     SLIDER_MAX_OVERFLOW: 0.05,
     DEBUG_MODE_TOGGLE: Object.freeze({
         REQUIRED_MIDDLE_CLICKS: 5,
         CLICK_WINDOW_MS: 2000
     }),
-    FALLBACK_LAYOUT: "ui"
-};
+    FALLBACK_LAYOUT: 'ui'
+});
